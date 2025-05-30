f985f5632d479102d96032795a0654c2903e42d6
Rajesh S <rajesha.s@gmail.com>
2025-05-30 20:34:31 +0530

    Handled The SVG color based on the theme

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -5,6 +5,7 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
+  className: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
@@ -13,6 +14,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'AI Assisted Circuit design',
     Svg: require('@site/static/img/ai-assist.svg').default,
+    className: "",
     description: (
       <>
         The Genie platform is designed to leverage AI for circuit design.
@@ -22,6 +24,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Optimize using AI',
     Svg: require('@site/static/img/ai-optimize.svg').default,
+    className: styles.overrideFillStroke,
     description: (
       <>
         AI reduces the time to Optimize your circuit.
@@ -31,6 +34,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Connect to multiple EDA tools',
     Svg: require('@site/static/img/design.svg').default,
+    className: "",
     description: (
       <>
         Genie platform has connectors to open source and commercial EDA tools.
@@ -39,11 +43,11 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg, className, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Svg className={styles.featureSvg + " " + className} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
